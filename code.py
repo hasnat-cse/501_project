32d974e7ff5300dc894da77b20d762f9b804d823
Arif Hasnat <hasnat@ualberta.ca>
2019-11-14 20:49:05 -0700

    sentival for english words

--- code.py
+++ code.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import re
 
+from nltk.corpus import wordnet as wn
+from nltk.corpus import sentiwordnet as swn
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+
 
 def preprocess_data():
     data = pd.read_csv('data/training_data.csv', encoding="latin1")
@@ -68,19 +74,76 @@ def read_word_label_file():
             sentences.append(sentence)
             sentence = []
 
-        words = line.split('\t')
+        else:
+            words = line.split('\t')
 
-        if words[0] != 'meta':
-            sentence.append(tuple(words))
-
-    print(len(sentences))
+            if len(words) == 2 and words[0] != 'meta':
+                sentence.append(tuple(words))
 
     return sentences
 
 
+def penn_to_wn(tag):
+    """
+    Convert between the PennTreebank tags to simple Wordnet tags
+    """
+    if tag.startswith('J'):
+        return wn.ADJ
+    elif tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('R'):
+        return wn.ADV
+    elif tag.startswith('V'):
+        return wn.VERB
+    return None
+
+
+def get_sentiment(word, tag):
+    """ returns list of pos neg and objective score. But returns empty list if not present in senti wordnet. """
+
+    wn_tag = penn_to_wn(tag)
+    if wn_tag not in (wn.NOUN, wn.ADJ, wn.ADV):
+        return []
+
+    lemmatizer = WordNetLemmatizer()
+
+    lemma = lemmatizer.lemmatize(word, pos=wn_tag)
+    if not lemma:
+        return []
+
+    synsets = wn.synsets(word, pos=wn_tag)
+    if not synsets:
+        return []
+
+    # Take the first sense, the most common
+    synset = synsets[0]
+    swn_synset = swn.senti_synset(synset.name())
+
+    return [swn_synset.pos_score(), swn_synset.neg_score(), swn_synset.obj_score()]
+
+
+def get_eng_words(sentence):
+    eng_words = []
+
+    for word_senti_tuple in sentence:
+        if word_senti_tuple[1] == "Eng":
+            eng_words.append(word_senti_tuple[0])
+
+    return eng_words
+
+
 def main():
-    # preprocess_data()
-    read_word_label_file()
+    # processed_texts = preprocess_data()
+
+    sentences = read_word_label_file()
+
+    ps = PorterStemmer()
+    for sentence in sentences:
+        words = get_eng_words(sentence)
+        pos_val = pos_tag(words)
+        senti_val = [get_sentiment(x, y) for (x, y) in pos_val]
+        print(words)
+        print(senti_val)
 
 
 if __name__ == "__main__":
